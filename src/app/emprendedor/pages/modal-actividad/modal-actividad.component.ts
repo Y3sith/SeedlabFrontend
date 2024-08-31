2dd767a189294ef2c7d7fd16f826c09a0efe628a
Evelyn <evelynestudioslp@gmail.com>
2024-08-31 13:00:28 -0500

    modal actividad ya trae descripcion, falta pulir diseño

--- src/app/emprendedor/pages/modal-actividad/modal-actividad.component.ts
+++ src/app/emprendedor/pages/modal-actividad/modal-actividad.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-actividad',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './modal-actividad.component.css'
 })
 export class ModalActividadComponent {
+  @Input() actividad: any;
+  @Input() visible: boolean = false;
+  @Output() close = new EventEmitter<void>();
 
+  imagenError: boolean = false;
+
+  cerrarModal() {
+    this.close.emit();
+  }
+
+  irAModulo() {
+    // Implementa la lógica para ir al módulo aquí
+    console.log('Ir al módulo de:', this.actividad.nombre);
+  }
+
+  manejarErrorImagen() {
+    this.imagenError = true;
+    console.error('Error al cargar la imagen:', this.actividad.imagen);
+  }
 }
