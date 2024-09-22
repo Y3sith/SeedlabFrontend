7940f33ab0234f1faa5937c7c50712be944621a2
Y3sith <145672787+Y3sith@users.noreply.github.com>
2024-09-22 16:43:33 -0500

    asesor quedo listo la ruta

--- src/app/asesor/pages/ruta-asesor/ruta-asesor.component.ts
+++ src/app/asesor/pages/ruta-asesor/ruta-asesor.component.ts
@@ -141,6 +141,7 @@ export class RutaAsesorComponent {
     this.validateToken();
 
     this.route.queryParams.subscribe(params => {
+      console.log('params:', params);
       if (params['id_ruta']) {
         this.rutaId = +params['id_ruta'];
         this.actividadForm.patchValue({ id_ruta: this.rutaId.toString() });
@@ -173,10 +174,6 @@ export class RutaAsesorComponent {
       this.onLeccionChange(idLeccion); // Llama la función que carga las lecciones
     }
     this.selectedFromInput = false;
-    if (idLeccion) {
-      this.onLeccionChange(idLeccion); // Llama la función que carga las lecciones
-    }
-    this.selectedFromInput = false;
   }
 
   validateToken(): void {
@@ -204,7 +201,9 @@ export class RutaAsesorComponent {
     if (this.token) {
       this.actividadService.getTipoDato(this.token).subscribe(
         data => {
+
           this.listarTipoDato = data.filter((tipo: any) => tipo.nombre === 'Imagen'); //solo me muestra imagen en el select tipo dato
+          // console.log('tipo de dato:', this.listarTipoDato);
         },
         error => {
           console.log(error);
@@ -222,7 +221,6 @@ export class RutaAsesorComponent {
       }
     });
   }
-
 
   goBack(): void {
     this.location.back();
@@ -246,6 +244,7 @@ export class RutaAsesorComponent {
       this.contenidoLeccionService.getTipoDato(this.token).subscribe(
         data => {
           this.listarTipoDatoContenido = data;
+          //console.log('tipo de dato contenido:', data);
         },
         error => {
           console.log(error);
@@ -275,6 +274,7 @@ export class RutaAsesorComponent {
 
   selectAliado(aliado: any): void {
     this.aliadoSeleccionado = aliado;
+    //console.log("el aliado seleccionado fue: ", this.aliadoSeleccionado)
   }
 
   onAliadoChange(event?: any): void {
@@ -288,11 +288,16 @@ export class RutaAsesorComponent {
 
     const aliadoSeleccionado = this.listarAliadoo.find(aliado => aliado.id == aliadoId);
     if (aliadoSeleccionado) {
+      console.log("El aliado seleccionado fue: ", {
+        id: aliadoSeleccionado.id,
+        nombre: aliadoSeleccionado.nombre
+      });
       this.aliadoSeleccionado = aliadoSeleccionado;
       if (this.token) {
         this.aliadoService.getinfoAsesor(this.token, this.aliadoSeleccionado.id, this.userFilter.estado).subscribe(
           data => {
             this.listarAsesores = data;
+            console.log('Asesores: ', data);
           },
           error => {
             console.log(error);
@@ -303,7 +308,6 @@ export class RutaAsesorComponent {
       console.error('No se encontró el aliado seleccionado');
     }
   }
-
 
   verEditar(): void {
     if (this.actividadId !== null) {
@@ -323,10 +327,13 @@ export class RutaAsesorComponent {
                 id_ruta: data.id_ruta,
               });
 
-              this.niveles = data.nivel;
+              //this.niveles = data.nivel;
               this.nivelForm.patchValue({ id_actividad: this.actividadId.toString() });
               this.selectedFromInput = false;
               this.initializeNivelForm();
+
+              // this.activivarFormulariosBotones();
+              console.log('Actividad: ', data);
             },
             error => {
               console.log('Error al cargar los asesores:', error);
@@ -349,15 +356,18 @@ export class RutaAsesorComponent {
         nombre: primerNivel.nombre,
         id_asesor: primerNivel.id_asesor
       });
+      //this.nivelForm.disable();
+      this.nivelForm.get('id_asesor')?.disable();
       // Cargar las lecciones del primer nivel
       this.onNivelChange(primerNivel.id.toString());
+      console.log('lecciones::', this.onNivelChange(primerNivel.id.toString()));
     } else {
       // Si no hay niveles, preparar para agregar uno nuevo
       this.nivelForm.patchValue({
         id_nivel: '',
         nombre: ''
       });
-      this.nivelForm.get('nombre')?.disable();
+      //this.nivelForm.get('nombre')?.disable();
     }
     if (this.contenidoLeccion && this.contenidoLeccion.length > 0) {
       const primerContenido = this.contenidoLeccion[0];
@@ -382,12 +392,12 @@ export class RutaAsesorComponent {
       return;
     }
 
-    if (this.actividadId != null) {
-      // if (this.actividadForm.invalid) {
-      //   this.alertServices.errorAlert('Error', 'Debes completar todos los campos requeridos de la actividad');
-      //   return;
-      // }
+    // if (this.actividadId != null) {
+    if (this.actividadForm.invalid) {
+      this.alertServices.errorAlert('Error', 'Debes completar todos los campos requeridos de la actividad');
+      return;
     }
+    // }
     if (this.idactividad == null) {
       estadoValue = '1'
     } else {
@@ -399,6 +409,7 @@ export class RutaAsesorComponent {
     formData.append('id_ruta', this.rutaId.toString());
     formData.append('id_aliado', this.actividadForm.get('id_aliado')?.value);
     formData.append('estado', estadoValue);
+    console.log('datos: ', this.actividadForm.value);
     if (this.selectedfuente) {
       formData.append('fuente', this.selectedfuente, this.selectedfuente.name);
     } else {
@@ -406,6 +417,7 @@ export class RutaAsesorComponent {
       if (rutaMultiValue) {
         formData.append('fuente', rutaMultiValue);
       }
+      console.log('datos enviados: ', formData)
     }
     if (this.actividadId == null) {
       this.alertServices.alertaActivarDesactivar("¿Estas seguro de guardar los cambios? Verifica los datos ingresados, una vez guardados solo se podran modificar en el apartado de editar", 'question').then((result) => {
@@ -416,6 +428,7 @@ export class RutaAsesorComponent {
               const actividadCreada = data[0];
               this.nivelForm.patchValue({ id_actividad: actividadCreada.id });
               this.alertServices.successAlert('Exito', data.message);
+              console.log('datos enviados: ', data)
             },
             error => {
               console.log(error);
@@ -441,13 +454,44 @@ export class RutaAsesorComponent {
       })
     }
   }
-  
+  // verNivel(): void {
+  //   if (this.token) {
+  //     this.nivelService.mostrarNivelXidActividad(this.token, parseInt(this.nivelForm.value.id_actividad)).subscribe(
+  //       data => {
+  //         this.listarNiveles = data;
+  //         this.niveles = data;
+  //         console.log('Niveles: ', data);
+
+
+  //         if (this.isEditing && this.niveles && this.niveles.length > 0) {
+  //           // this.nivelForm.patchValue({
+  //           //   id_nivel: this.niveles[0].id,
+  //           //   nombre: this.niveles[0].nombre
+  //           // });
+  //           const primerNivel = this.niveles[0];
+  //                   this.nivelForm.patchValue({
+  //                       id_nivel: primerNivel.id,
+  //                       nombre: primerNivel.nombre
+  //                   });
+  //                   // Llamar a onNivelChange para actualizar las lecciones
+  //                   this.onNivelChange(primerNivel.id.toString());
+  //         }
+  //       },
+  //       error => {
+  //         console.log(error);
+  //       }
+  //     )
+  //   }
+  // }
   verNivel(): void {
     if (this.token) {
       this.nivelService.mostrarNivelxidActividadxidAsesor(this.token, this.actividadId, this.idAsesor).subscribe(
         data => {
           this.listarNiveles = data;
           this.niveles = data;
+          console.log('Niveles: ', data);
+
+
 
           if (this.isEditing && this.niveles && this.niveles.length > 0) {
             const primerNivel = this.niveles[0];
@@ -494,7 +538,9 @@ export class RutaAsesorComponent {
       id_asesor: idAsesor,
       //id_asesor: this.nivelForm.value.id_asesor,
       id_actividad: this.nivelForm.value.id_actividad
+      //id_actividad: this.actividadId
     };
+    console.log("idnivel", this.selectedNivelId);
     if (this.nivelForm.value.id_nivel && this.nivelForm.value.id_nivel !== '0') {
       const nivelId = this.nivelForm.get('id_nivel')?.value;
       this.nivelService.updateNivel(this.token, nivelId, nivel).subscribe(
@@ -520,37 +566,38 @@ export class RutaAsesorComponent {
         }
       );
     } else {
-      if (this.nivelForm.invalid) {
-        this.alertServices.errorAlert('Error', 'Debes completar todos los campos requeridos del nivel');
-        return;
-      }
-      this.superAdminService.crearNivelSuperAdmin(this.token, nivel).subscribe(
-        (data: any) => {
-          this.alertServices.successAlert('Exito', data.message);
-          // Actualizar la lista de niveles
-          this.niveles.push({
-            id: data.id,
-            nombre: data.nombre
-          });
+      // if (this.nivelForm.invalid) {
+      //   this.alertServices.errorAlert('Error', 'Debes completar todos los campos requeridos del nivel');
+      //   return;
+      // }
+      // console.log('nivel data', nivel);
+      // this.superAdminService.crearNivelSuperAdmin(this.token, nivel).subscribe(
+      //   (data: any) => {
+      //     this.alertServices.successAlert('Exito', data.message);
+      //     // Actualizar la lista de niveles
+      //     this.niveles.push({
+      //       id: data.id,
+      //       nombre: data.nombre
+      //     });
 
-          // Actualizar el select
-          this.nivelForm.patchValue({
-            id_nivel: data.id
-          });
-          this.leccionForm.patchValue({ id_nivel: data.id })
-          this.verNivel();
-          this.verLeccicon();
-          this.nivelForm.reset();
-          this.submittedNivel = false;
-          this.nivelForm.patchValue({ id_actividad: nivel.id_actividad });
-          this.alertServices.successAlert('Éxito', 'Nivel creado correctamente')
-          this.onNivelChange(data.id.toString());
-        },
-        error => {
-          this.alertServices.errorAlert('Error', error.error.message);
-          console.log(error);
-        }
-      )
+      //     // Actualizar el select
+      //     this.nivelForm.patchValue({
+      //       id_nivel: data.id
+      //     });
+      //     this.leccionForm.patchValue({ id_nivel: data.id })
+      //     this.verNivel();
+      //     this.verLeccicon();
+      //     this.nivelForm.reset();
+      //     this.submittedNivel = false;
+      //     this.nivelForm.patchValue({ id_actividad: nivel.id_actividad, id_asesor: idAsesor });
+      //     this.alertServices.successAlert('Éxito', 'Nivel creado correctamente')
+      //     this.onNivelChange(data.id.toString());
+      //   },
+      //   error => {
+      //     this.alertServices.errorAlert('Error', error.error.message);
+      //     console.log(error);
+      //   }
+      // )
     }
   }
 
@@ -570,16 +617,20 @@ export class RutaAsesorComponent {
       nombre: nombreLeccion,
       id_nivel: this.leccionForm.value.id_nivel
     }
+    //const leccionId = +this.leccionForm.get('id_leccion')?.value;
     const leccionId = +this.leccionForm.get('id_leccion')?.value;
     if (leccionId) {
+      //console.log("leccionIddddddddd", leccionId);
       const leccionId = +this.leccionForm.get('id_leccion')?.value;
       this.leccionService.updateLeccion(this.token, leccionId, leccion).subscribe(
         (data) => {
           this.alertServices.successAlert('Exito', data.message);
           this.onNivelChange(this.leccionForm.value.id_nivel);
+          //this.verLeccicon();
           this.leccionForm.reset();
           this.submittedLeccion = false;
           this.leccionForm.patchValue({ id_nivel: leccion.id_nivel });
+          console.log('id leccion: ', data.id);
         },
         error => {
           this.alertServices.errorAlert('Error', error.error.message);
@@ -587,14 +638,20 @@ export class RutaAsesorComponent {
         }
       )
     } else {
+      console.log('leccion data', leccion);
       this.superAdminService.crearLeccionSuperAdmin(this.token, leccion).subscribe(
         (data: any) => {
+          console.log('datos recibidos', data);
           this.alertServices.successAlert('Exito', data.message);
           this.onNivelChange(this.leccionForm.value.id_nivel);
           this.contenidoLeccionForm.patchValue({ id_leccion: data.id })
+          //this.verLeccicon();
+          //this.mostrarContenidoLeccionForm = true;
+          //this.mostrarContenidoLeccionForm = true;
           this.leccionForm.reset();
           this.submittedLeccion = false;
           this.leccionForm.patchValue({ id_nivel: leccion.id_nivel });
+          console.log('id leccion: ', data.id);
         },
         error => {
           this.alertServices.errorAlert('Error', error.error.message);
@@ -688,12 +745,16 @@ export class RutaAsesorComponent {
       })
       this.contenidoLeccion = [];
     }
+
+    console.log('id_leccion actual en leccionForm:', this.leccionForm.get('id_leccion').value);
+    console.log('id_leccion actual en contenidoLeccionForm:', this.contenidoLeccionForm.get('id_leccion').value);
   }
 
   cargarContenidoLeccion(id_leccion: number): void {
     this.contenidoLeccionService.contenidoXleccion(this.token, id_leccion).subscribe(
       data => {
         this.contenidoLeccion = data;
+        console.log('Contenido de la lección:', data);
         if (this.isEditing && data.length > 0) {
           const primerContenido = data[0];
           this.contenidoLeccionForm.patchValue({
@@ -721,28 +782,27 @@ export class RutaAsesorComponent {
     const selectedNivelId = event.target.value;
     this.selectedNivelId = selectedNivelId !== '0' ? parseInt(selectedNivelId) : null;
     if (selectedNivelId === '0') {
-      if (selectedNivelId === '0') {
-        this.nivelForm.patchValue({ nombre: '', id_nivel: 0 });
-        this.nivelForm.patchValue({ id_actividad: this.actividadId.toString() });
-        this.contenidoLeccionForm.patchValue({
-          titulo: '',
-          descripcion: '',
-          id_contenido: '',
-          id_leccion: '',
-          id_tipo_dato: ''
-        })
-        this.contenidoLeccion = [];
-      } else {
-        const selectedNivel = this.niveles.find(nivel => nivel.id === parseInt(selectedNivelId));
-        if (selectedNivel) {
-          this.nivelForm.patchValue({
-            id_nivel: selectedNivel.id,
-            nombre: selectedNivel.nombre
-          });
-        }
+      this.nivelForm.patchValue({ nombre: '', id_nivel: 0 });
+      this.nivelForm.patchValue({ id_actividad: this.actividadId.toString() });
+      this.contenidoLeccionForm.patchValue({
+        titulo: '',
+        descripcion: '',
+        id_contenido: '',
+        id_leccion: '',
+        id_tipo_dato: ''
+      })
+      this.contenidoLeccion = [];
+    } else {
+      const selectedNivel = this.niveles.find(nivel => nivel.id === parseInt(selectedNivelId));
+      if (selectedNivel) {
+        this.nivelForm.patchValue({
+          id_nivel: selectedNivel.id,
+          nombre: selectedNivel.nombre
+        });
       }
     }
   }
+
   addContenidoLeccionSuperAdmin(): void {
     this.submittedContent = true
     // if (this.contenidoLeccionForm.invalid) {
@@ -762,7 +822,6 @@ export class RutaAsesorComponent {
       return;
     }
 
-
     const formData = new FormData();
     formData.append('id_leccion', idLeccion);
     let estadoValue: string;
@@ -773,7 +832,9 @@ export class RutaAsesorComponent {
     formData.append('titulo', this.contenidoLeccionForm.get('titulo')?.value);
     formData.append('descripcion', this.contenidoLeccionForm.get('descripcion')?.value);
     formData.append('id_tipo_dato', this.contenidoLeccionForm.get('id_tipo_dato')?.value);
+    //formData.append('id_leccion', this.contenidoLeccionForm.get('id_leccion')?.value);
     formData.append('id_leccion', idLeccion);
+    console.log('id_leccion a enviar:', idLeccion);
 
     if (this.selectedfuenteContenido) {
       formData.append('fuente_contenido', this.selectedfuenteContenido, this.selectedfuenteContenido.name);
@@ -785,12 +846,15 @@ export class RutaAsesorComponent {
     }
     const contenidoLeccionId = +this.contenidoLeccionForm.get('id_contenido')?.value;
     if (contenidoLeccionId) {
+      console.log("conteido", contenidoLeccionId)
       this.contenidoLeccionService.updateContenidoLeccion(this.token, contenidoLeccionId, formData).subscribe(
         (data) => {
           this.alertServices.successAlert('Exito', data.message);
+          console.log('datos recibidos: ', data);
           this.cargarContenidoLeccion(contenidoLeccionId);
           this.contenidoLeccionForm.reset();
           this.submittedContent = false;
+          //location.reload();
         },
         error => {
           this.alertServices.errorAlert('Error', error.error.message);
@@ -801,9 +865,12 @@ export class RutaAsesorComponent {
       this.superAdminService.crearContenicoLeccionSuperAdmin(this.token, formData).subscribe(
         (data: any) => {
           this.alertServices.successAlert('Exito', data.message);
+          console.log('datos recibidos: ', data);
           this.cargarContenidoLeccion(+idLeccion);
           this.contenidoLeccionForm.reset();
           this.submittedContent = false;
+
+          //location.reload();
         },
         error => {
           this.alertServices.errorAlert('Error', error.error.message);
@@ -814,7 +881,6 @@ export class RutaAsesorComponent {
   }
   onContenidoSelect(contenidoId: string): void {
     const currentIdLeccion = this.contenidoLeccionForm.get('id_leccion').value;
-
 
     if (contenidoId === 'nuevo') {
       // Si se selecciona "Agregar contenido nuevo"
@@ -921,6 +987,7 @@ export class RutaAsesorComponent {
   onTextInputContenido(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
     this.contenidoLeccionForm.patchValue({ fuente_contenido: value });
+    //console.log('fuente actualizada:', value);  // Para depuración
   }
 
   triggerFileInputContenido() {
@@ -972,4 +1039,7 @@ export class RutaAsesorComponent {
     this.selectedfuenteContenido = null;
     this.fuentePreviewContenido = null;
   }
+
+  ////////////////////////////////////////////////////////////////////////
+
 }
