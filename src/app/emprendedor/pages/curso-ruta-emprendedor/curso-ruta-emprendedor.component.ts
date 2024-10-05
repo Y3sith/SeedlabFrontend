df382dc7a866cd0a727b870165bd4818af73a2a5
Marly Rangel <rangelmarly@gmail.com>
2024-10-05 18:56:27 -0500

    perfiles en movil

--- src/app/emprendedor/pages/curso-ruta-emprendedor/curso-ruta-emprendedor.component.ts
+++ src/app/emprendedor/pages/curso-ruta-emprendedor/curso-ruta-emprendedor.component.ts
@@ -294,57 +294,6 @@ export class CursoRutaEmprendedorComponent {
     });
   }
 
-/*
-  Avanza al siguiente contenido en la lección actual.
-*/
-  // goToNextContent() {
-  //   if (this.showActivityDescription) {
-  //     this.showActivityDescription = false;
-  //     this.currentNivelIndex = 0;
-  //     this.currentLeccionIndex = 0;
-  //     this.currentContenidoIndex = 0;
-  //   } else {
-  //     const currentNivel = this.niveles[this.currentNivelIndex];
-  //     const currentLeccion = currentNivel.lecciones[this.currentLeccionIndex];
-  //     const nextContenidoIndex = this.currentContenidoIndex + 1;
-
-  //     if (nextContenidoIndex < currentLeccion.contenido_lecciones.length) {
-  //       this.currentContenidoIndex = nextContenidoIndex;
-  //     } else {
-  //       const lastContentId = currentLeccion.contenido_lecciones[currentLeccion.contenido_lecciones.length - 1].id;
-  //       if (lastContentId === this.ultimoContenidoId) {
-  //         if (this.currentRolId != 1 && this.currentRolId != 2) {
-  //           this.alertService.alertainformativa('¡Felicitaciones por haber completado la ruta! Ahora es momento de volver a llenar la encuesta de maduración. Esta te permitirá evaluar cuánto has avanzado con tu emprendimiento y cómo han influido los conocimientos que has adquirido en este tiempo. Es una gran oportunidad para reflexionar sobre tu progreso y seguir mejorando. ¡Sigue adelante!', 'success').then((result) => {
-  //             if (result.isConfirmed) {
-  //               this.router.navigate(['list-empresa']);
-  //             }
-  //           });
-  //         } else {
-  //           this.router.navigate(['ruta']);
-  //         }
-  //         return;
-  //       }
-
-  //       this.currentLeccionIndex++;
-  //       if (this.currentLeccionIndex >= currentNivel.lecciones.length) {
-  //         this.currentLeccionIndex = 0;
-  //         this.currentNivelIndex++;
-  //         if (this.currentNivelIndex >= this.niveles.length) {
-  //           this.botonAsesoria = true;
-  //           this.router.navigate(['ruta']);
-  //           return;
-  //         }
-  //       }
-  //       this.currentContenidoIndex = 0;
-  //     }
-  //   }
-  //   const newCurrentNivel = this.niveles[this.currentNivelIndex];
-  //   const newCurrentLeccion = newCurrentNivel.lecciones[this.currentLeccionIndex];
-  //   const newCurrentContenido = newCurrentLeccion.contenido_lecciones[this.currentContenidoIndex];
-  //   this.closeAllExceptSelected(this.currentNivelIndex, this.currentLeccionIndex, newCurrentContenido.id);
-  //   this.selectedContenido = newCurrentContenido;
-  //   this.updateSelectedContent();
-  // }
 
   goToNextContent() {
     if (this.showActivityDescription) {
