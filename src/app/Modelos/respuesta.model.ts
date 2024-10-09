46421f02d3140e417fe75741122ef5b56f3b216f
Marly <rangelmarly@gmail.com>
2024-10-09 12:07:29 -0500

    opcion en modelo nullable

--- src/app/Modelos/respuesta.model.ts
+++ src/app/Modelos/respuesta.model.ts
@@ -10,7 +10,7 @@ export class Respuesta {
   constructor({id_pregunta, id_empresa,  opcion, texto_res, valor,  fecha_reg,  id_subpregunta}
     :{id_pregunta?: number, id_empresa?: number,  opcion?: string, texto_res?: string, valor?: number,  fecha_reg?: Date,  id_subpregunta?: number}) {
     this.opcion = opcion;
-    this.texto_res = texto_res;
+    this.texto_res = texto_res ?? '';
     this.valor = valor ?? 0;
     this.fecha_reg = fecha_reg ?? new Date();
     this.id_pregunta = id_pregunta;
